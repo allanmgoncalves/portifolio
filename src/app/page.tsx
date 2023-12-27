cf06e976236c6d5398f397b0d0a333e8fb974962
Allan Gonçalves <allangoncalves.adm@gmail.com>
2023-12-27 11:05:21 -0300

    Update class order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,9 +95,9 @@ export default function Home() {
 
               <label
                 htmlFor="photo"
-                className="hover:bg-violet-25 group flex flex-1 cursor-pointer flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-zinc-500 shadow-sm hover:border-violet-200 hover:text-violet-500"
+                className="group flex flex-1 cursor-pointer flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25 hover:text-violet-500"
               >
-                <div className="border-6 rounded-full border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
+                <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
                   <UploadCloud className="h-5 w-5 text-zinc-600 group-hover:text-violet-600" />
                 </div>
                 <div className="flex flex-col items-center gap-1">
